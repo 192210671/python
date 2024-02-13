feebc81b5a171d6cbaf9e588a0a68f183d143c3c
192210671 <124900140+192210671@users.noreply.github.com>
2024-02-13 08:43:28 +0530

    Create slicing array.py

---
+++ practise programms/day3/slicing array.py
@@ -0,0 +1,12 @@
+#slicng arrays
+letters=['a','b','c','d','e','f']
+letters
+#to change
+letters[2:5]=['C','D','E']
+letters
+# to temove
+letters[2:5]=[]
+letters
+#to make empty
+letters[:]=[]
+letters
