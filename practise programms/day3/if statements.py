060e6fd6e94c77db51f57b7073b3e3fd06be3273
192210671 <124900140+192210671@users.noreply.github.com>
2024-02-13 09:16:15 +0530

    Create if statements.py

---
+++ practise programms/day3/if statements.py
@@ -0,0 +1,11 @@
+#if statements
+n=int(input("Enter the Number="))
+if x < 0:
+  x=0
+  print('Negative Changed to Zero')
+elif x == 0:
+  print('zero')
+elif x == 1:
+  print('single')
+else:
+  print('More')
