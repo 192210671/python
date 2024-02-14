1a258627607995472ab79e0c38247dfdf0fd907b
192210671 <124900140+192210671@users.noreply.github.com>
2024-02-14 12:58:54 +0530

    Create kwarguments.py

---
+++ practise programms/day4/kwarguments.py
@@ -0,0 +1,9 @@
+def parrot(voltage,action='voom',state='stiff',type='norwegian blue'):
+
+  print("-----------This parrot wouldn't",action,end='')
+  print("---------if you put",voltage,"volts throught it")
+  print("Lovely plumage ,the ",type)
+  print("it's state is ",state)
+
+
+parrot(1000,action='noob',state='fly',type='pro')
