3c2cdbace6a88ea14aa443ddde2a50b9d53e7ab4
192210671 <124900140+192210671@users.noreply.github.com>
2024-02-14 14:04:31 +0530

    Create basic ds.py

---
+++ practise programms/day4/basic ds.py
@@ -0,0 +1,14 @@
+#datastrucutres
+
+list=["apple","banana","grapes","strawberry","apple"]
+list.count("apple")
+list.count("banana")
+list.append("jackfruit")
+list
+list.insert(3,"plum")
+list
+list.pop(5)
+list
+list.reverse()
+list
+list.copy()
