4baec0a1333f5e4e74ca282bf5c96c2d76d46f43
Eswar Chinthakayala <124900140+192210671@users.noreply.github.com>
2024-03-08 10:42:58 +0530

    Create square root.py

---
+++ practise programms/codedman/square root.py
@@ -0,0 +1,12 @@
+
+import math
+
+def square_root(number):
+    x=math.sqrt(number)
+    return x
+
+
+# Sample Tests
+print(square_root(9)) # should return 3
+print(square_root(81)) # should return 9
+
