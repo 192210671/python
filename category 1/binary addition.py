77b23a80245f034e78d60ce82995066c2612f525
Eswar Chinthakayala <124900140+192210671@users.noreply.github.com>
2024-03-14 10:53:31 +0530

    Create binary addition.py

---
+++ category 1/binary addition.py
@@ -0,0 +1,6 @@
+#sum of binary numbers
+a=input("Enter the first binary number=")
+b=input("Enter the second binary number=")
+
+sum=bin(int(a,2)+int(b,2))
+print("Sum of binary number is sum=",sum[2:])
