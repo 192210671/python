3c801da2184f7a67aa7cbff036dbb0e4404be854
Eswar Chinthakayala <124900140+192210671@users.noreply.github.com>
2024-03-27 23:50:56 +0530

    Create sum of two matrices.py

---
+++ practise++/sum of two matrices.py
@@ -0,0 +1,14 @@
+mat1=[[1,2,3],
+      [4,5,6],
+      [7,8,9]]
+mat2=[[9,8,7],
+      [6,5,4],
+      [3,2,1]]
+result=[[0,0,0],[0,0,0],[0,0,0]]
+
+for i in range(len(mat1)):
+      for j in range(len(mat1[0])):
+            result[i][j]=mat1[i][j]+mat2[i][j]
+print("Sum of two matrices is:")
+for i in result:
+      print(i)
