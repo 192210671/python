a4934aa1a5434ed2db6595e5fc4485cf56b43ed9
Eswar Chinthakayala <124900140+192210671@users.noreply.github.com>
2024-03-27 23:57:02 +0530

    Create multiplication of matrix.py

---
+++ practise++/multiplication of matrix.py
@@ -0,0 +1,19 @@
+x = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+y = [[9, 8, 7],
+     [6, 5, 4],
+     [3, 2, 1]]
+
+result = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+for i in range(len(x)):
+      for j in range(len(y[0])):
+            for k in range(len(y)):
+                  result[i][j]+=x[i][k]*y[k][j]
+
+for i in result:
+      print(i)
