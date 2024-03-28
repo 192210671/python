aca15bebf2b302885c58c492a747141cbe332dbf
Eswar Chinthakayala <124900140+192210671@users.noreply.github.com>
2024-03-28 23:55:48 +0530

    Create operations on sets.py

---
+++ practise++/operations on sets.py
@@ -0,0 +1,7 @@
+set1={1,2,3,4,5}
+set2={4,5,6,7,8}
+
+print("Union of Two sets:",set1|set2)
+print("Intersection of Two Sets:",set1&set2)
+print("Difference:",set1-set2)
+print("Symmetric Difference:",set1^set2)
